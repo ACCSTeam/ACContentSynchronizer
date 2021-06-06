38121e2637ac65a61c5213386bba7a837e811a9a
fEst <chubenko98@mail.ru>
2021-06-06 17:50:49 +0600

    removed cli better server content update restart server after update if server empty

---
+++ ACContentSynchronizer.ClientGui/Windows/UploadContent.axaml.cs
@@ -0,0 +1,33 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Markup.Xaml;
+
+namespace ACContentSynchronizer.ClientGui.Windows {
+  public class UploadContentWindow : Window {
+    private readonly UploadContentViewModel _vm;
+
+    public UploadContentWindow() {
+      DataContext = _vm = new();
+
+      InitializeComponent();
+#if DEBUG
+      this.AttachDevTools();
+#endif
+    }
+
+    private void InitializeComponent() {
+      AvaloniaXamlLoader.Load(this);
+    }
+
+    public static void Open(Window parent, string http) {
+      var wnd = new UploadContentWindow();
+      wnd._vm.Http = http;
+      wnd.ShowDialog(parent);
+    }
+
+    private void Close(object? sender, RoutedEventArgs e) {
+      Close();
+    }
+  }
+}
