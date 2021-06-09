34eab957eefeebc16100887a02b1455f2ec902c2
fEst <chubenko98@mail.ru>
2021-06-09 17:15:16 +0600

    solid color brush transition

--- ACContentSynchronizer.ClientGui/Styles/SolidColorBrushTransition.cs
+++ ACContentSynchronizer.ClientGui/Styles/SolidColorBrushTransition.cs
@@ -2,29 +2,18 @@ using Avalonia.Animation;
 using Avalonia.Media;
 using System;
 using System.Reactive.Linq;
+using Avalonia.Animation.Animators;
 
 namespace ACContentSynchronizer.ClientGui.Styles {
-  public class SolidColorBrushTransition : Transition<IBrush> {
-    public override IObservable<IBrush> DoTransition(IObservable<double> progress, IBrush oldValue, IBrush newValue) {
-      if (oldValue is not ISolidColorBrush oldBrush) {
-        throw new ArgumentException("Only instances of ISolidColorBrush are supported", nameof(oldValue));
-      }
-      if (newValue is not ISolidColorBrush newBrush) {
-        throw new ArgumentException("Only instances of ISolidColorBrush are supported", nameof(newValue));
-      }
-
-      var oldColor = oldBrush.Color;
-      var newColor = newBrush.Color;
-
-      return progress.Select(p => {
-        var e = Easing.Ease(p);
-        var a = (int) (e * (newColor.A - oldColor.A) + 0.5) + oldColor.A;
-        var r = (int) (e * (newColor.R - oldColor.R) + 0.5) + oldColor.R;
-        var g = (int) (e * (newColor.G - oldColor.G) + 0.5) + oldColor.G;
-        var b = (int) (e * (newColor.B - oldColor.B) + 0.5) + oldColor.B;
-
-        return new SolidColorBrush(new Color((byte) a, (byte) r, (byte) g, (byte) b));
-      });
+  public class BrushAnimator : Animator<IBrush> {
+    public override SolidColorBrush Interpolate(double progress, IBrush oldIValue, IBrush newIValue) {
+      var oldValue = (SolidColorBrush) oldIValue;
+      var newValue = (SolidColorBrush) newIValue;
+      var color = new ColorAnimator().Interpolate(progress, oldValue.Color, newValue.Color);
+      return new(color);
     }
+  }
+
+  public class SolidColorBrushTransition : AnimatorDrivenTransition<IBrush, BrushAnimator> {
   }
 }
